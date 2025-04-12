d2303d37f322df2b07fd05fe46cca40a30a49311
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2025-04-12 19:04:25 +0300

    REFACT- remove release

--- lcd_for_nuvoton/GCC/Release/CMSIS/CMSIS/GCC/_syscalls.d
+++
@@ -1,2 +0,0 @@
-CMSIS/CMSIS/GCC/_syscalls.o: \
- C:/Users/ibrahim/Desktop/M031BSP-master/Library/Device/Nuvoton/M031/Source/GCC/_syscalls.c
--- lcd_for_nuvoton/GCC/Release/CMSIS/CMSIS/GCC/startup_M031Series.d
+++
@@ -1,2 +0,0 @@
-CMSIS/CMSIS/GCC/startup_M031Series.o: \
- C:/Users/ibrahim/Desktop/M031BSP-master/Library/Device/Nuvoton/M031/Source/GCC/startup_M031Series.S
